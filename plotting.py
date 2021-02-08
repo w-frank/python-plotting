95041d94289f92ebb9bedd9fc9332c887d0b98f9
Will Frank <will_frank@hotmail.co.uk>
2021-02-08 12:01:41 +0000

    Improve comments and add LaTeX installation instructions

--- plotting.py
+++ plotting.py
@@ -1,3 +1,17 @@
+"""Python Plotting
+
+A custom Python class to adjust the default Matplotlib settings so that all 
+figures have a uniform format and look.
+
+To install LaTeX on Ubuntu 20.04 Focal Fossa Linux:
+    sudo apt install texlive-latex-extra
+    sudo apt install cm-super
+    sudo apt install dvipng
+
+    usage: from plotting import custom_plotting
+
+"""
+
 class custom_plotting:
     def setup_plots(self,  fontsize=8, usetex=True):
         """
@@ -5,11 +19,14 @@ class custom_plotting:
         figures have the same feel.
 
         Note that with usetex=True, fonts are rendered with LaTeX. This may
-        result in an error if LaTeX is not installed. In that case you can set usetex to False.
+        result in an error if LaTeX is not installed on the system. In that case
+        you can set usetex to False.
         """
         import matplotlib
+
         self.fontsize = fontsize
         self.usetex = usetex
+
         matplotlib.rc('legend', fontsize=self.fontsize, handlelength=3)
         matplotlib.rc('axes', titlesize=self.fontsize)
         matplotlib.rc('axes', labelsize=self.fontsize)
--- plotting_example.py
+++ plotting_example.py
@@ -1,3 +1,17 @@
+"""Python Plotting Example
+
+This shows an example of a Poisson distribution with various parameters. The
+custom plotting class is used to style the figure.
+
+To install LaTeX on Ubuntu 20.04 Focal Fossa Linux:
+    sudo apt install texlive-latex-extra
+    sudo apt install cm-super
+    sudo apt install dvipng
+
+    usage: python3 plotting_example.py
+
+"""
+
 import numpy as np
 from scipy.stats import poisson
 from matplotlib import pyplot as plt
@@ -7,14 +21,14 @@ from plotting import custom_plotting
 custom_plotting = custom_plotting()
 custom_plotting.setup_plots(fontsize=10, usetex=True)
 
-
 # Define the distribution parameters to be plotted
 mu_values = [1, 5, 15]
 linestyles = ['-', '--', ':']
 
 """
 plot the distributions
-we generate it using scipy.stats.poisson(). Once the distribution object is created, we have many options: for example:
+we generate it using scipy.stats.poisson(). Once the distribution object is
+created, we have many options: for example:
     - dist.pmf(x) evaluates the probability mass function in the case of
       discrete distributions.
     - dist.pdf(x) evaluates the probability density function for
@@ -31,9 +45,8 @@ for mu, ls in zip(mu_values, linestyles):
     dist = poisson(mu)
     x = np.arange(-1, 200)
 
-    plt.plot(x, dist.pmf(x), color='black',
-             linestyle='steps-mid' + ls,
-             label=r'$\mu=%i$' % mu)
+    plt.plot(x, dist.pmf(x), color='black', ds='steps-mid', ls=ls,
+                label=r'$\mu=%i$' % mu)
 
 plt.xlim(-0.5, 30)
 plt.ylim(0, 0.4)
